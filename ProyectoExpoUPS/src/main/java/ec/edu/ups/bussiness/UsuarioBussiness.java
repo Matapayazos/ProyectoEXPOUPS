1af4f0a4ac4793ad3002a3a25386d199d8539ef9
CCastillocd <39130277+CCastillocd@users.noreply.github.com>
2019-08-06 23:37:42 -0500

    Update UsuarioBussiness.java

--- ProyectoExpoUPS/src/main/java/ec/edu/ups/bussiness/UsuarioBussiness.java
+++ ProyectoExpoUPS/src/main/java/ec/edu/ups/bussiness/UsuarioBussiness.java
@@ -8,12 +8,25 @@ import javax.inject.Inject;
 import ec.edu.ups.dao.UsuarioDAO;
 import ec.edu.ups.model.Usuario;
 
+/**
+ * Establece las reglas de negocio y las validaciones necesarias para la entidad
+ * Usuario
+ * @author Cancer31
+ *
+ */
 @Stateless
 public class UsuarioBussiness {
 
 	@Inject
 	private UsuarioDAO dao;
 
+	/**
+	 * Método para guardar Usuarios
+	 * @param usuario nuevo usuario de tipo usuario
+	 * @returnretorna true si se ha agregado corectamente un aspirante
+	 * @throws Exception indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones).
+	 */
 	public boolean save(Usuario usuario) throws Exception {
 		Usuario aux = dao.read(usuario.getId());
 		if (aux != null)
@@ -24,11 +37,21 @@ public class UsuarioBussiness {
 		return true;
 	}
 
+	/**
+	 * lista de usuarios
+	 * @return retorna una lista de usuarios
+	 */
 	public List<Usuario> getUsuarios() {
 
 		return dao.getUsuarios();
 	}	
 
+	/**
+	 * Método para eliminar un usuario
+	 * @param id id del usuario de tipo int 
+	 * @throws Exception indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones).
+	 */
 	public void delete(int id) throws Exception {
 		Usuario aux = dao.read(id);
 
@@ -38,6 +61,13 @@ public class UsuarioBussiness {
 			dao.remove(id);
 	}
 
+	/**
+	 * Método para actualizar usuarios
+	 * @param usuario de tipo usuario
+	 * @returnretorna true si se ha actualizado corectamente un aspirante
+	 * @throws Exception indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones).
+	 */
 	public boolean update(Usuario usuario) throws Exception {
 		Usuario aux = dao.read(usuario.getId());
 		if (aux == null)
@@ -48,6 +78,13 @@ public class UsuarioBussiness {
 		return true;
 	}
 
+	/**
+	 * listar usaurios
+	 * @param id
+	 * @return
+	 * @throws Exception indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones).
+	 */
 	public Usuario getUsuario(int id) throws Exception {
 		Usuario aux = dao.read(id);
 		if (aux == null)
