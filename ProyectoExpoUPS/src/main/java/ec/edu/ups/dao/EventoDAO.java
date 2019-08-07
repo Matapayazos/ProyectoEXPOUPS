eb3357cd8fdca275a084f8666dc56c683e9168ec
CCastillocd <39130277+CCastillocd@users.noreply.github.com>
2019-08-06 23:52:13 -0500

    Update EventoDAO.java

--- ProyectoExpoUPS/src/main/java/ec/edu/ups/dao/EventoDAO.java
+++ ProyectoExpoUPS/src/main/java/ec/edu/ups/dao/EventoDAO.java
@@ -9,32 +9,59 @@ import javax.persistence.Query;
 import ec.edu.ups.model.Evento;
 import ec.edu.ups.model.Evento;
 
+/**
+ * Etidad que permite realizar todas las operaciones de mantenimiento (C.R.U.D)
+ * sobre la entidad  Evento
+ * @author Cancer31
+ *
+ */
 @Stateless
 public class EventoDAO {
 
 	@Inject
 	private EntityManager em;
 
+	/**
+	 * 
+	 * @param evento
+	 */
 	public void insert(Evento evento) {
 		em.persist(evento);
 
 	}
 
+	/**
+	 * 
+	 * @param evento
+	 */
 	public void update(Evento evento) {
 		em.merge(evento);
 
 	}
 
+	/**
+	 * 
+	 * @param id
+	 */
 	public void remove(int id) {
 		em.remove(this.read(id));
 
 	}
 
+	/**
+	 * lsiata eventos por id
+	 * @param id id del evento 
+	 * @return retorna un evento 
+	 */
 	public Evento read(int id) {
 		Evento evento = em.find(Evento.class, id);
 		return evento;
 	}
 
+	/**
+	 * lista de eventos
+	 * @return retoena una lista de eventos
+	 */
 	public List<Evento> getEventos() {
 
 		String jpql = "SELECT c FROM Evento c";
@@ -43,6 +70,11 @@ public class EventoDAO {
 		List<Evento> listado = query.getResultList();
 		return listado;
 	}
+	/**
+	 * listar eventos por fecha
+	 * @param fecha fecha para buscar
+	 * @return retoena una lsita de eventos 
+	 */
 	public List<Evento> readxFecha(Date fecha) {
 
 		String jpql = "SELECT c FROM Evento c WHERE c.fecha=:fecha";
