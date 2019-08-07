efb75f5bb3e84f3c745ddeedcac2ba6384b4a61c
CCastillocd <39130277+CCastillocd@users.noreply.github.com>
2019-08-06 22:29:23 -0500

    Update CarreraBussiness.java

--- ProyectoExpoUPS/src/main/java/ec/edu/ups/bussiness/CarreraBussiness.java
+++ ProyectoExpoUPS/src/main/java/ec/edu/ups/bussiness/CarreraBussiness.java
@@ -15,11 +15,25 @@ import ec.edu.ups.dao.CarreraDAO;
 import ec.edu.ups.model.Carrera;
 import ec.edu.ups.model.Carrera;
 
+/**
+ * Establece las reglas de negocio y las validaciones necesarias para la entidad
+ * Carrera
+ * @author Cancer31
+ *
+ */
 @Stateless
 public class CarreraBussiness {
 	
 	@Inject
 	private CarreraDAO dao;
+	
+	/**
+	 * Método para guardar carreras
+	 * @param carrera la nueva carrera
+	 * @return retorna true si se ha agregado corectamente un aspirante
+	 * @throws Exception indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones).
+	 */
 
 	public boolean save(Carrera carrera) throws Exception {
 		Carrera aux = dao.read(carrera.getId());
@@ -29,11 +43,22 @@ public class CarreraBussiness {
 			dao.insert(carrera);
 		return true;
 	}
+	
+	/**
+	 * Lista de Aspirantes
+	 * @return Retorna una lista de carreras
+	 */
 
 	public List<Carrera> getCarreras() {
 		return dao.getCarreras();
 	}
 
+	/**
+	 * Método para eliminar un carrera por medio de su id
+	 * @param id de la carrera de tipo int
+	 * @throws Exception indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones).
+	 */
 	public void delete(int id) throws Exception {
 		Carrera aux = dao.read(id);
 
@@ -43,6 +68,13 @@ public class CarreraBussiness {
 			dao.remove(id);
 	}
 
+	/**
+	 * Método para actualizar una carrera
+	 * @param carrera de tipo carrera
+	 * @return retorna true si se ha actualizado corectamente
+	 * @throws Exception Exception indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones).
+	 */
 	public boolean update(Carrera carrera) throws Exception {
 		Carrera aux = dao.read(carrera.getId());
 		if (aux == null)
@@ -52,6 +84,13 @@ public class CarreraBussiness {
 		return true;
 	}
 
+	/**
+	 * Metodo para listar carrera
+	 * @param id
+	 * @return
+	 * @throws Exception Exception indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones).
+	 */
 	public Carrera getCarrera(int id) throws Exception {
 		Carrera aux = dao.read(id);
 		if (aux == null)
