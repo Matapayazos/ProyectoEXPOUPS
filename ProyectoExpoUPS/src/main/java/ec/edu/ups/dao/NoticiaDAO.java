5ecd8d1c8bf9eb5bffaf1ba0ed991e05b17c906b
CCastillocd <39130277+CCastillocd@users.noreply.github.com>
2019-08-07 00:06:47 -0500

    Update NoticiaDAO.java

--- ProyectoExpoUPS/src/main/java/ec/edu/ups/dao/NoticiaDAO.java
+++ ProyectoExpoUPS/src/main/java/ec/edu/ups/dao/NoticiaDAO.java
@@ -8,32 +8,59 @@ import javax.persistence.Query;
 
 import ec.edu.ups.model.Noticia;
 
+/**
+ * Etidad que permite realizar todas las operaciones de mantenimiento (C.R.U.D)
+ * sobre la entidad noticia
+ * @author Cancer31
+ *
+ */
 @Stateless
 public class NoticiaDAO {
 
 	@Inject
 	private EntityManager em;
 
+	/**
+	 * 
+	 * @param noticia
+	 */
 	public void insert(Noticia noticia) {
 		em.persist(noticia);
 
 	}
 
+	/**
+	 * 
+	 * @param noticia
+	 */
 	public void update(Noticia noticia) {
 		em.merge(noticia);
 
 	}
 
+	/**
+	 * 
+	 * @param id
+	 */
 	public void remove(int id) {
 		em.remove(this.read(id));
 
 	}
 
+	/**
+	 * listar noticia por id
+	 * @param id id de la noticia
+	 * @return retorna una noticia
+	 */
 	public Noticia read(int id) {
 		Noticia noticia = em.find(Noticia.class, id);
 		return noticia;
 	}
 
+	/**
+	 * lista de noticias
+	 * @return retorna una lista de noticias
+	 */
 	public List<Noticia> getNoticias() {
 
 		String jpql = "SELECT c FROM Noticia c";
