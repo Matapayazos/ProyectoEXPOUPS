ea664383ee3ccee8695c8e8240ba8c14d034e444
CCastillocd <39130277+CCastillocd@users.noreply.github.com>
2019-08-07 00:09:01 -0500

    Update UsuarioDAO.java

--- ProyectoExpoUPS/src/main/java/ec/edu/ups/dao/UsuarioDAO.java
+++ ProyectoExpoUPS/src/main/java/ec/edu/ups/dao/UsuarioDAO.java
@@ -9,31 +9,58 @@ import javax.persistence.Query;
 
 import ec.edu.ups.model.Usuario;
 
+/**
+ * Etidad que permite realizar todas las operaciones de mantenimiento (C.R.U.D)
+ * sobre la entidad Usuario
+ * @author Cancer31
+ *
+ */
 @Stateless
 public class UsuarioDAO {
 
 	@Inject
 	private EntityManager em;
 
+	/**
+	 * 
+	 * @param usuario
+	 */
 	public void insert(Usuario usuario) {
 		em.persist(usuario);
 
 	}
 
+	/**
+	 * 
+	 * @param usuario
+	 */
 	public void update(Usuario usuario) {
 		em.merge(usuario);
 
 	}
 
+	/**
+	 * 
+	 * @param id
+	 */
 	public void remove(int id) {
 		em.remove(this.read(id));
 
 	}
 
+	/**
+	 * listar usuarios por id
+	 * @param id id del usuario
+	 * @return retorna un usuario
+	 */
 	public Usuario read(int id) {
 		Usuario usuario = em.find(Usuario.class, id);
 		return usuario;
 	}
+	/**
+	 * lista de usuarios
+	 * @return retorna una lista de usuarios
+	 */
 	
 	public List<Usuario> getUsuarios() {
 
