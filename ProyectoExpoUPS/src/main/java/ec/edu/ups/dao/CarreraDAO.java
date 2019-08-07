953e4d296abb8240d1071054fd6e406391021c23
CCastillocd <39130277+CCastillocd@users.noreply.github.com>
2019-08-06 23:48:07 -0500

    Update CarreraDAO.java

--- ProyectoExpoUPS/src/main/java/ec/edu/ups/dao/CarreraDAO.java
+++ ProyectoExpoUPS/src/main/java/ec/edu/ups/dao/CarreraDAO.java
@@ -10,32 +10,59 @@ import javax.persistence.Query;
 import ec.edu.ups.model.Carrera;
 import ec.edu.ups.model.Carrera;
 
+/**
+ * Etidad que permite realizar todas las operaciones de mantenimiento (C.R.U.D)
+ * sobre la entidad  Carrera
+ * @author Cancer31
+ *
+ */
 @Stateless
 public class CarreraDAO {
 
 	@Inject
 	private EntityManager em;
 
+	/**
+	 * 
+	 * @param carrera
+	 */
 	public void insert(Carrera carrera) {
 		em.persist(carrera);
 
 	}
 
+	/**
+	 * 
+	 * @param carrera
+	 */
 	public void update(Carrera carrera) {
 		em.merge(carrera);
 
 	}
 
+	/**
+	 * 
+	 * @param id
+	 */
 	public void remove(int id) {
 		em.remove(this.read(id));
 
 	}
 
+	/**
+	 * listar carrera por id 
+	 * @param id id de la carrera
+	 * @return retona una carrera
+	 */
 	public Carrera read(int id) {
 		Carrera carrera = em.find(Carrera.class, id);
 		return carrera;
 	}
 
+	/**
+	 * listar carreras
+	 * @return retorna una lista de carreras
+	 */
 	public List<Carrera> getCarreras() {
 		String jpql = "SELECT c FROM Carrera c";
 		Query query = em.createQuery(jpql, Carrera.class);
