7de7c3c9b11dbc450b3d939095032f048d698c22
CCastillocd <39130277+CCastillocd@users.noreply.github.com>
2019-08-06 22:40:57 -0500

    Update EventoBussiness.java

--- ProyectoExpoUPS/src/main/java/ec/edu/ups/bussiness/EventoBussiness.java
+++ ProyectoExpoUPS/src/main/java/ec/edu/ups/bussiness/EventoBussiness.java
@@ -18,10 +18,24 @@ import ec.edu.ups.model.Evento;
 import ec.edu.ups.model.Evento;
 
 
+/**
+ * Establece las reglas de negocio y las validaciones necesarias para la entidad
+ * Eventos
+ * @author Cancer31
+ *
+ */
 @Stateless
 public class EventoBussiness {
 	@Inject
 	private EventoDAO dao;
+	
+	/**
+	 * Método para guarda eventos
+	 * @param evento nuevo evento de tipo evento
+	 * @return retorna true si se ha agregado corectamente
+	 * @throws Exception Exception indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones).
+	 */
 
 	public boolean save(Evento evento) throws Exception {
 		Evento aux = dao.read(evento.getId());
@@ -33,11 +47,21 @@ public class EventoBussiness {
 		return true;
 	}
 
+	/**
+	 * Lista de eventos
+	 * @return Retorna una lista de eventos
+	 */
 	public List<Evento> getEventos() {
 
 		return dao.getEventos();
 	}
 
+	/**
+	 * Método para eliminar un evento
+	 * @param id id del evento de tipo int
+	 * @throws Exception indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones).
+	 */
 	public void delete(int id) throws Exception {
 		Evento aux = dao.read(id);
 
@@ -47,6 +71,13 @@ public class EventoBussiness {
 			dao.remove(id);
 	}
 
+	/**
+	 * Método para actualizar eventos
+	 * @param evento evento para actualizar de tipo evento
+	 * @return retorna true si se ha actualizado corectamente
+	 * @throws Exception indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones).
+	 */
 	public boolean update(Evento evento) throws Exception {
 		Evento aux = dao.read(evento.getId());
 		if (aux == null)
@@ -57,6 +88,13 @@ public class EventoBussiness {
 		return true;
 	}
 
+	/**
+	 * Listar Eventos 
+	 * @param id
+	 * @return
+	 * @throws Exception  indica que este método es capaz de lanzar una
+	 * Exception del tipo Exception (padre de todas las excepciones)
+	 */
 	public Evento getEvento(int id) throws Exception {
 		Evento aux = dao.read(id);
 		if (aux == null)
@@ -64,6 +102,11 @@ public class EventoBussiness {
 		else
 			return aux;
 	}
+	/**
+	 * Lista de eventos por fecha
+	 * @param fecha
+	 * @return retorna una lista de eventos
+	 */
 	public List<Evento> getEventosXFecha(Date fecha) {
 
 		return dao.getEventoXFecha(fecha);
